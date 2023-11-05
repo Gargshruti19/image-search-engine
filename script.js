9d53f316ab13e2bec5e2ddf1c212b0f75e113f49
Shruti Garg <138093537+Gargshruti19@users.noreply.github.com>
2023-11-05 15:44:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const accessKey = "WCO1Nkc4C26M4kW-Oi6JOlTCYpPVQG1BTAkKrgN9tKM";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+const searchBtn = document.querySelector("#srch");
+let keyword = "";
+let page = 91;
+
+async function searchImages() {
+	keyword = searchBox.value;
+	const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+	const response = await fetch(url);
+	const data = await response.json();
+
+	if (page === 1) {
+		searchResult.innerHTML = "";
+	}
+
+	const results = data.results;
+
+	results.map((result) => {
+		const image = document.createElement("img");
+		image.src = result.urls.small;
+
+		const imageLink = document.createElement("a");
+		imageLink.href = result.links.html;
+
+		imageLink.target = "_blank";
+
+		imageLink.appendChild(image);
+		searchResult.appendChild(imageLink);
+	});
+
+	showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", function (e) {
+	e.preventDefault();
+	page = 1;
+
+	searchImages();
+});
+
+showMoreBtn.addEventListener("click", function () {
+	page++;
+	searchImages();
+	console.log(":btn click");
+});
+
+searchBtn.addEventListener("click", function () {
+	searchImages();
+});
